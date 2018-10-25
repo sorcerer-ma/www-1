c1685f49f0f22956f83def4b80fb516d11e2ed43
lixueli <lixueli@qiniu.com>
2018-10-25 16:18:36 +0800

    Init site

---
+++ src/pages/index.js
@@ -0,0 +1,32 @@
+import React from 'react'
+// import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+import Image from '../components/image'
+
+const IndexPage = () => (
+  <Layout>
+    <h2 id="section-a">Section A</h2>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Now go build something great.</p>
+    <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
+      <Image />
+    </div>
+
+    <h2 id="section-b">Section B</h2>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Now go build something great.</p>
+    <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
+      <Image />
+    </div>
+
+    <h2 id="section-c">Section C</h2>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Now go build something great.</p>
+    <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
+      <Image />
+    </div>
+  </Layout>
+)
+
+export default IndexPage
